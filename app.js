daa486a9ca40b436386116a730854eb4d9c175be
Funnypp <2392261894@qq.com>
2017-03-16 18:02:08 +0800

    express two: add views

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-var express = require('express');
+var express = require('express'),
+    path = require('path'),
+    handlebars = require('express-handlebars').create({ defaultLayout: 'main' });   // 指明了默认布局，非特别指明，所有的视图用的都是这个布局
+
+var routes = require('./routes');
 
 var app = express();
 
+// 设置port
 app.set('port', process.env.PORT || 3000);
+// 创建视图引擎
+app.engine('handlebars', handlebars.engine);
+// 设置视图路径
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'handlebars');
+
+routes(app);
 
 app.listen(app.get('port'), function() {
     console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
---
+++ routes/index.js
@@ -0,0 +1,26 @@
+/**
+ * 路由
+ */
+module.exports = function(app) {
+
+    app.get('/', function(req, res, next) {
+       res.render('home');
+    });
+
+    app.get('/about', function(req, res, next) {
+       res.render('about');
+    });
+
+    // 404 catch-all 处理器（中间件）
+    app.use(function(req, res, next) {
+        res.status(404);
+        res.render('404');
+    });
+
+    // 500 错误处理器（中间件）
+    app.use(function(err, req, res, next) {
+        console.error(err.stack);
+        res.status(500);
+        res.render('500');
+    });
+};
